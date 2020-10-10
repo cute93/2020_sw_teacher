908cdd3b7707cf1b096713a5cb2cb15a9f03a27c
HandLake <biismark3@gmail.com>
2020-10-10 16:02:58 +0900

    firstday

--- pointclass.py
+++ pointclass.py
@@ -6,9 +6,20 @@ class piPoint:
         return f'[{self.x}, {self.y}]'
     def dist(self, other):
         return ((other.x-self.x)**2+(other.y-self.y)**2)**0.5
+    
+    def mathPi(self):
+        import math
+        return math.pi
+    def leibniz(self):
+        pass
+    def auler(self):
+        pass
+    def archimedes(self):
+        return 227/27
         
 if __name__=='__main__':
     p = piPoint(3,4)
     print(p)
     print(p, piPoint())
     print(p.dist(piPoint()))
+    print(p.actualPi())
